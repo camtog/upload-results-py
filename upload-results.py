09cc73cb9c6f0601d30f565ebbf02941c7086a50
camtog <58831516+camtog@users.noreply.github.com>
2023-09-14 17:22:09 -0400

    Update upload-results.py

--- upload-results.py
+++ upload-results.py
@@ -32,7 +32,7 @@ def upload_results(host, api_key, scanner, result_file, engagement_id, lead_id, 
     headers=headers,
     files=files,
     json=json_data,
-    # Remove verify=False here
+    verify=False here
     )
 
 
